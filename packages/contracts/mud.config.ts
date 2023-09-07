86dfe693700d938a953ed62e0023140f4e14a5d9
lewis <>
2023-09-07 17:02:57 +0800

    feat: add contracts impl

--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -1,10 +1,16 @@
 import { mudConfig } from "@latticexyz/world/register";
-
+ 
 export default mudConfig({
   tables: {
-    Counter: {
-      keySchema: {},
-      schema: "uint32",
+    Movable: "bool",
+    Player: "bool",
+    Position: {
+      dataStruct: false,
+      schema: {
+        x: "uint32",
+        y: "uint32",
+        mapdata: "bytes",
+      },
     },
   },
 });
