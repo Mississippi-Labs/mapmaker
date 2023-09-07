215fb759fb9d3237a5d5a22a14ce211c712a6932
lewis <>
2023-09-07 17:45:29 +0800

    feat: add map make impl

---
+++ packages/contracts/src/systems/MapSystem.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+import { System } from "@latticexyz/world/src/System.sol";
+import { Movable, Player, Position } from "../codegen/Tables.sol";
+import { addressToEntityKey } from "../addressToEntityKey.sol";
+ 
+contract MapSystem is System {
+  function spawn(uint32 x, uint32 y) public {
+    bytes32 player = addressToEntityKey(address(_msgSender()));
+    require(!Player.get(player), "already spawned");
+
+    Player.set(player, true);
+    Position.set(player, x, y, "");
+    Movable.set(player, true);
+  }
+
+  function getData() public view returns(uint32, uint32, bytes memory) {
+    bytes32 player = addressToEntityKey(address(_msgSender()));
+    require(Player.get(player), "already spawned");
+
+    return Position.get(player);
+  }
+
+  function move(uint32 x, uint32 y, bytes calldata data) public {
+    bytes32 player = addressToEntityKey(_msgSender());
+    require(Movable.get(player), "cannot move");
+ 
+    (uint32 fromX, uint32 fromY, bytes memory d) = Position.get(player);
+    require(distance(fromX, fromY, x, y) == 1, "can only move to adjacent spaces");
+ 
+    Position.set(player, x, y, data);
+  }
+ 
+  function distance(uint32 fromX, uint32 fromY, uint32 toX, uint32 toY) internal pure returns (uint32) {
+    uint32 deltaX = fromX > toX ? fromX - toX : toX - fromX;
+    uint32 deltaY = fromY > toY ? fromY - toY : toY - fromY;
+    return deltaX + deltaY;
+  }
+}
